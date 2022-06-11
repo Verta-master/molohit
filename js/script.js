4336347b259bc713e0a1636b5d6147916f905fb9
Verta-master <vertyagina@gmail.com>
2022-06-11 19:15:27 +0600

    Update 2022-06-11T13:15:23.415Z

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 //Mobile menu
-$('.menu__open').click(function() {
-  $(this).toggleClass('menu__open--close');
-  $('.menu__list').slideToggle();
-})
+//$('.menu__open').click(function() {
+//  $(this).toggleClass('menu__open--close');
+//  $('.menu__list').slideToggle();
+//})
 
 if (window.innerWidth < 1023) {
-  $('.menu__btn').click(function() {
-    $(this).next().slideToggle();
-    $(this).toggleClass('menu__btn--top');
-  });
-  $('.menu__next').click(function() {
-    $(this).next().slideToggle();
-    $(this).toggleClass('menu__next--top');
-  });
+//  $('.menu__btn').click(function() {
+//    $(this).next().slideToggle();
+//    $(this).toggleClass('menu__btn--top');
+//  });
+//  $('.menu__next').click(function() {
+//    $(this).next().slideToggle();
+//    $(this).toggleClass('menu__next--top');
+//  });
   $('.footer__nav .footer__title').click(function() {
     $(this).next().slideToggle();
     $(this).toggleClass('footer__title--top');
@@ -20,7 +20,7 @@ if (window.innerWidth < 1023) {
 }
 
 //Search
-if (window.innerWidth < 481) {
+if (window.innerWidth < 1023) {
   $('.search__open').click(function() {
     $(this).toggleClass('search__open--close');
      $('.search__form').slideToggle();
