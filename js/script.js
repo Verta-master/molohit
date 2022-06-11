190b4a4828b1ae633594315d5fbc094dcce57e8f
Verta-master <vertyagina@gmail.com>
2022-06-11 18:06:01 +0600

    adaptive

--- js/script.js
+++ js/script.js
@@ -1,18 +1,33 @@
 //Mobile menu
-//$('.menu__btn').click(function() {
-//  $('.menu').toggleClass('menu--opened');
-//  $('.menu__list').slideToggle();
-//})
+$('.menu__open').click(function() {
+  $(this).toggleClass('menu__open--close');
+  $('.menu__list').slideToggle();
+})
 
-//if (window.innerWidth < 766) {
-//  $('.menu__link').click(function() {
-//    $('.menu__list').slideToggle();
-//    $('.menu').removeClass('menu--opened');
-//  })
-//}
+if (window.innerWidth < 1023) {
+  $('.menu__btn').click(function() {
+    $(this).next().slideToggle();
+    $(this).toggleClass('menu__btn--top');
+  });
+  $('.menu__next').click(function() {
+    $(this).next().slideToggle();
+    $(this).toggleClass('menu__next--top');
+  });
+  $('.footer__nav .footer__title').click(function() {
+    $(this).next().slideToggle();
+    $(this).toggleClass('footer__title--top');
+  })
+}
 
 //Search
-$('.search__open').click(function() {
-  $(this).toggleClass('search__open--close');
-  $('.search__form').toggleClass('search__form--show');
-})
+if (window.innerWidth < 481) {
+  $('.search__open').click(function() {
+    $(this).toggleClass('search__open--close');
+     $('.search__form').slideToggle();
+  })
+} else {
+  $('.search__open').click(function() {
+    $(this).toggleClass('search__open--close');
+    $('.search__form').toggleClass('search__form--show');
+  });
+}
