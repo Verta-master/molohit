8b3f06acfc2b87a984527635a158ccd23c1110a9
Verta-master <vertyagina@gmail.com>
2022-06-13 18:33:32 +0600

    Update 2022-06-13T12:33:28.031Z

---
+++ js/subcategory.js
@@ -0,0 +1,34 @@
+function postsCarousel() {
+  var checkWidth = $(window).width();
+  var owlPost = $(".subcategory__list");
+  
+  if (checkWidth > 1200) {
+    if (typeof owlPost.data('owl.carousel') != 'undefined') {
+      owlPost.data('owl.carousel').destroy();
+    }
+    owlPost.removeClass('owl-carousel');
+  } else if (checkWidth < 1201) {
+    owlPost.addClass('owl-carousel');
+    owlPost.owlCarousel({
+      loop: true,
+      margin: 20,
+      nav: true,
+      dots: false,
+      navText:["",""],
+      responsive:{
+        0:{
+          items: 2
+        },
+        480:{
+          items: 3,
+        },
+        766:{
+          items: 4,
+        }
+      }
+    });
+  }
+}
+
+postsCarousel();
+$(window).resize(postsCarousel);	
